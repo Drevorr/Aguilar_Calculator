30532d4f0a9968724d2d4b0b883732eb41292826
Drevorr <vinceangeloaguilar@gmail.com>
2022-03-18 00:29:55 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/aguilar_calculator/MainActivity.java
@@ -0,0 +1,171 @@
+package mcm.edu.ph.aguilar_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import org.mariuszgromada.math.mxparser.*;
+import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText display;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.textView);
+        display.setShowSoftInputOnFocus(false);
+
+        display.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getString(R.string.display).equals(display.getText().toString()))
+                    display.setText("");
+            }
+
+        });
+
+    }
+
+    private void updateText(String strToAdd) {
+        String oldStr = display.getText().toString();
+        int cursorPos = display.getSelectionStart();
+        String leftStr = oldStr.substring(0, cursorPos);
+        String rightStr = oldStr.substring(cursorPos);
+        if (getString(R.string.display).equals(display.getText().toString())) {
+            display.setText(strToAdd);
+            display.setSelection(cursorPos + 1);
+        } else {
+            display.setText(String.format("%s%s%s", leftStr, strToAdd, rightStr));
+            display.setSelection(cursorPos + 1);
+
+        }
+
+    }
+
+    public void zerobutton(View view) {
+        updateText("0");
+    }
+
+    public void onebutton(View view) {
+        updateText("1");
+    }
+
+    public void twobutton(View view) {
+        updateText("2");
+    }
+
+    public void threebutton(View view) {
+        updateText("3");
+    }
+
+    public void fourbutton(View view) {
+        updateText("4");
+    }
+
+    public void fivebutton(View view) {
+        updateText("5");
+    }
+
+    public void sixbutton(View view) {
+        updateText("6");
+    }
+
+    public void sevenbutton(View view) {
+        updateText("7");
+    }
+
+    public void eightbutton(View view) {
+        updateText("8");
+    }
+
+    public void ninebutton(View view) {
+        updateText("9");
+    }
+
+    public void dividebutton(View view) {
+        updateText("÷");
+    }
+
+    public void multiplybutton(View view) {
+        updateText("×");
+    }
+
+    public void addbutton(View view) {
+        updateText("+");
+    }
+
+    public void subtractbutton(View view) {
+        updateText("-");
+    }
+
+    public void clearbutton(View view) {
+        display.setText("");
+    }
+
+    public void parenthesesbutton(View view) {
+        int cursorPos = display.getSelectionStart();
+        int openPar = 0;
+        int closedPar = 0;
+        int textLen = display.getText().length();
+
+        for (int i = 0; i < cursorPos; i++) {
+            if (display.getText().toString().substring(i, i+1).equals("(")) {
+                openPar += 1;
+            }
+            if (display.getText().toString().substring(i, i+1).equals(")")) {
+                closedPar += 1;
+            }
+        }
+
+        if (openPar == closedPar || display.getText().toString().substring(textLen-1, textLen).equals("(")) {
+            updateText("(");
+        }
+        else if (closedPar < openPar && !display.getText().toString().substring(textLen-1, textLen).equals("(")) {
+            updateText(")");
+        }
+        display.setSelection(cursorPos + 1);
+    }
+
+    public void exponentbutton (View view) {
+        updateText("^");
+    }
+
+    public void plusMinusbutton (View view) {
+        updateText("-");
+    }
+
+    public void decimalbutton (View view) {
+        updateText(".");
+    }
+
+    public void equalbutton (View view) {
+        String userExp = display.getText().toString();
+
+        userExp = userExp.replaceAll("÷", "/");
+        userExp = userExp.replaceAll("×", "*");
+
+        Expression exp = new Expression(userExp);
+
+        String result = String.valueOf(exp.calculate());
+
+        display.setText(result);
+        display.setSelection(result.length());
+
+    }
+
+    public void backspacebutton (View view){
+        int cursorPos = display.getSelectionStart();
+        int textLen = display.getText().length();
+
+        if (cursorPos != 0 && textLen != 0) {
+            SpannableStringBuilder selection = (SpannableStringBuilder) display.getText();
+            selection.replace(cursorPos - 1, cursorPos, "");
+            display.setText(selection);
+            display.setSelection(cursorPos - 1);
+        }
+    }
+}
